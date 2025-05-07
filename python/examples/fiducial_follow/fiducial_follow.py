7f783f8401e90d1e3fb253cf9c538d3356689458
Abhishek Patil <abhishek.dilip.patil@uni-weimar.de>
2025-05-07 16:43:31 +0200

    distance margin changed from 0.5 to 0.15

--- python/examples/fiducial_follow/fiducial_follow.py
+++ python/examples/fiducial_follow/fiducial_follow.py
@@ -547,7 +547,7 @@ def main():
 
     parser = argparse.ArgumentParser()
     bosdyn.client.util.add_base_arguments(parser)
-    parser.add_argument('--distance-margin', default=.5,
+    parser.add_argument('--distance-margin', default=.15, #default value by boston dynamic was 0.5
                         help='Distance [meters] that the robot should stop from the fiducial.')
     parser.add_argument('--limit-speed', default=True, type=lambda x: (str(x).lower() == 'true'),
                         help='If the robot should limit its maximum speed.')
