ee4e563b1ddbe54bc25541757e7660c31bd2db1d
Abhishek Patil <abhishek.dilip.patil@uni-weimar.de>
2025-06-17 12:51:21 +0200

    run program command added

---
+++ python/self-programs/run_cmd.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+
+"""Script to run basic program on a robot using the Spot SDK."""
+
+# Configuration
+program_directory = os.path.dirname(os.path.abspath(__file__))
+program_name = "basic_program.py"
+robot_ip = "192.168.80.3"
+
+# Change to the directory where the program is located
+os.chdir(program_directory)
+
+# Run the program
+# subprocess.Popen('cmd')
+subprocess.run(["python3", program_name, robot_ip])
